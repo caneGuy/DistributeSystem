5817849d342f33b1d2e930b817a508205f8ac57c
zhoukang <zhoukang199191@gmail.com>
2017-09-14 15:40:57 +0800

    Add file header

--- LeaseProtocol/src/main/java/cane/distribute/lease/LeaseMaster.java
+++ LeaseProtocol/src/main/java/cane/distribute/lease/LeaseMaster.java
@@ -17,26 +17,5 @@
 
 package cane.distribute.lease;
 
-public class LeaseClient {
-
-    private Lease _holdLease;
-
-    public void requestLease(String host, int port) {
-
-    }
-
-    public void doCall(LeaseCallBack callBack) {
-        //TODO: check lease is expired or not
-        callBack.call();
-    }
-
-    private void renewLease(String host, int port) {
-
-    }
-
-    class LeaseUpdater implements Runnable {
-        public void run() {
-
-        }
-    }
+public class LeaseMaster {
 }
